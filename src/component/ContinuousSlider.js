cc00d931a09e2cd66b002e56f14d0a13e094ec65
Arjun <arjun.drag007@gmail.com>
2024-06-14 10:45:27 +0530

    Added required files

---
+++ src/component/ContinuousSlider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+
+const ContinuousSlider = () => {
+  return (
+    <> <Slider defaultValue={20}/></>
+  )
+}
+
+export default ContinuousSlider
---
+++ src/component/DiscreteSlider.js
@@ -0,0 +1,11 @@
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import React from 'react'
+
+const DiscreteSlider = () => {
+  return (
+    <>  <Slider step={10} dots /></>
+  )
+}
+
+export default DiscreteSlider
